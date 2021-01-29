3163baaad4621bf07bd3d73440a5139139ba5e12
DirtyDan00 <47438008+DirtyDan00@users.noreply.github.com>
2021-01-29 00:04:11 -0700

    Timer and Score
    Created a working timer and Score for collectables, implemented collectables

---
+++ Capstone Proj/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreScript : MonoBehaviour
+{
+    Text currentScore;
+    public static int scoreValue;
+    private void Start()
+    {
+        currentScore = GetComponent<Text>();
+    }
+    void Update()
+    {
+        currentScore.text = "Score: " + scoreValue;
+    }
+
+}
---
+++ Capstone Proj/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+using UnityEngine.UI;
+public class TimerScript : MonoBehaviour
+{
+    Text TimerUI;
+    private float hoursCount;
+    private float minuteCount;
+    private float secondsCount;
+    private float milisecondsCount;
+    private void Start()
+    {
+       TimerUI = GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+        milisecondsCount = Time.deltaTime * 1000;
+    }
+
+    private void Timer()
+    {
+        secondsCount += Time.deltaTime;
+        TimerUI.text = "Timer " + minuteCount + ":" + (int)secondsCount + ":" + milisecondsCount;
+        if(milisecondsCount >= 100)
+        {
+            secondsCount++;
+            milisecondsCount = 0;
+        }
+        else if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+    }
+}
